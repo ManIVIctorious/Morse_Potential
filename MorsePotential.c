de32f1eb6a8bc9946464bd31e05c97fcdb428543
Mani Victorious <ManIVIctorious@gmail.com>
2016-01-28 15:29:32 +0100

    add "optarg" options

--- MorsePotential.c
+++ MorsePotential.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <gsl/gsl_sf.h>
+#include <getopt.h>
 
 int factorial(int n);
 
@@ -17,7 +18,115 @@ int main(int argc, char **argv){
     double xmin = -2.5;             // angstrom
     double xmax =  2.5;             // angstrom
     char *outputfile = "/dev/stdout";
+    static int kcal_flag = 0; // set flag to kcal/mol
     int numberofeigenstates=6;
+
+    int c;
+    while(1){
+        static struct option long_options[] = {
+            /* These options set a flag. */
+            {"kcal", no_argument, &kcal_flag, 1},
+            /* These options don’t set a flag.
+               We distinguish them by their indices. */
+            {"help",                  no_argument,       0, 'h'},
+            {"xmin",                  required_argument, 0, 'a'},
+            {"xmax",                  required_argument, 0, 'b'},
+            {"x-spacing",             required_argument, 0, 'd'},
+            {"well-depth",            required_argument, 0, 'D'},
+            {"potential-width",       required_argument, 0, 'w'},
+            {"reduced-mass",          required_argument, 0, 'm'},
+            {"output-file",           required_argument, 0, 'o'},
+            {"number-of-eigenstates", required_argument, 0, 'n'},
+            {0, 0, 0, 0}
+        };
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "a:b:d:D:w:m:o:n:h", long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+
+        switch(c){
+            case 0:
+            /* If this option set a flag, do nothing else now. */
+            if (long_options[option_index].flag != 0)
+                break;
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
+
+            case 'h':
+                printf("%s\t", argv[0]);
+                printf("[--help|-h]");
+                printf("[--xmin|-a <value>]");
+                printf("[--xmax|-b <value>]");
+                printf("\n\t\t\t");
+                printf("[--x-spacing|-d <value>]");
+                printf("[--well-depth|-D <value>]");
+                printf("\n\t\t\t");
+                printf("[--potential-width|-w <value>]");
+                printf("[--output-file|-o]");
+                printf("\n\t\t\t");
+                printf("[--number-of-eigenstates|-n]");
+                printf("[--kcal]");
+                printf("\n\t\t\t");
+                printf("[--reduced-mass|-m <value>]");
+                printf("\n\n");
+                
+                printf("-h, --help\t\t\tShow this help dialogue\n");
+                printf("-a, --xmin\t\t\tSet minimum x-value\n");
+                printf("-b, --xmax\t\t\tSet maximum x-value\n");
+                printf("-d, --x-spacing\t\t\tSet spacing between x-values\n");
+                printf("-D, --well-depth\t\tSet the D factor for well-depth in kJ/mol\n");
+                printf("-w, --potential-width\t\tSet the alpha parameter for potential width\n");
+                printf("-m, --reduced-mass\t\tSet reduced mass of involved particles in g/mol\n");
+                printf("-o, --output-file\t\tName of output file\n");
+                printf("-n, --number-of-eigenstates\tNumber of calculated eigenstates\n");
+                printf("    --kcal\t\t\tOutput in kcal/mol\n");
+                printf("\n");
+                exit (0);
+
+            case 'a':
+                //printf ("option -a with value `%s'\n", optarg);
+                xmin = atof(optarg);
+                break;
+
+            case 'b':
+                xmax = atof(optarg);
+                break;
+
+            case 'd':
+                dx = atof(optarg);
+                break;
+
+            case 'D':
+                D = atof(optarg);
+                break;
+
+            case 'w':
+                alpha = atof(optarg);
+                break;
+
+            case 'm':
+                mu = atof(optarg);
+                break;
+
+            case 'o':
+                outputfile = optarg;
+                break;
+
+            case 'n':
+                numberofeigenstates = atoi(optarg);
+                break;
+
+            default:
+                abort();
+        }
+    }
 
     // static declaration
     int n;
@@ -41,6 +150,12 @@ int main(int argc, char **argv){
     EvalTerm1 =           1.0E10/(2.0*pi)*avogadro * planck*alpha*sqrt(2.0*D/mu);   // kJ/mol
     EvalTerm2 = 1.0E20/(8*pi*pi)*avogadro*avogadro * planck*planck*alpha*alpha/mu;  // kJ/mol
 
+    // convert to kcal/mol
+    if(kcal_flag == 1){
+        EvalTerm1 /=4.184;
+        EvalTerm2 /=4.184;
+    }
+
     // get eigenvalues
     for(n=0; n<numberofeigenstates; ++n){
         E[n] = EvalTerm1*((double)n + 0.5) - EvalTerm2*((double)n*(double)n + (double)n + 0.25);
@@ -54,11 +169,23 @@ int main(int argc, char **argv){
         N[n] = sqrt(((double)factorial(n)*(2.0*lambda-2.0*(double)n-1.0))/gsl_sf_gamma(2.0*lambda - (double)n));
     }
 
+    fprintf(fd, "#D = %lf kJ/mol\n", D);
+    fprintf(fd, "#alpha = %lf\n", alpha);
+    fprintf(fd, "#reduced mass = %lf g/mol\n", mu);
+    if(kcal_flag==1){
+    fprintf(fd, "#EvalTerm1 = %lf kcal/mol\n", EvalTerm1);
+    fprintf(fd, "#EvalTerm2 = %lf kcal/mol\n", EvalTerm2);
+    }else{
     fprintf(fd, "#EvalTerm1 = %lf kJ/mol\n", EvalTerm1);
     fprintf(fd, "#EvalTerm2 = %lf kJ/mol\n", EvalTerm2);
+    }
     fprintf(fd, "#lambda = %lf\n", lambda);
     for(n=0; n<numberofeigenstates; ++n){
-        fprintf(fd, "#N%d = % 26.20lf\t E%d = % 26.20lf\n", n, N[n], n, E[n]);
+        if(kcal_flag == 1){
+        fprintf(fd, "#N%d = % 26.20lf\t E%d = % 26.20lf kcal/mol\n", n, N[n], n, E[n]);
+        }else{
+        fprintf(fd, "#N%d = % 26.20lf\t E%d = % 26.20lf kJ/mol\n", n, N[n], n, E[n]);
+        }
     }
 
     // Calculate E+Psi and output data
