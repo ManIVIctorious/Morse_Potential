d9fe56a0a8de80587b207c62195eba1f0c99d0f9
Mani Victorious <ManIVIctorious@gmail.com>
2016-01-28 11:24:57 +0100

    first working version of morse oscillator

--- main.c
+++ main.c
@@ -7,20 +7,24 @@
 int main(int argc, char **argv){
 
     // Morse Potential: V(x) = D*(1-exp(-alpha*(x-x0)))^2
-    // dynamic declaration
+    // dynamic variables
     double D = 426.369999999928;    // kJ/mol
     double alpha = 2.511;           // 1/angstrom
-    double mu = 1;
-    double dx = 0.025;
-    double xmin = -2.5;
-    double xmax =  2.5;
+    double mu = 1;                  // g/mol
+    double dx = 0.025;              // angstrom
+    double xmin = -2.5;             // angstrom
+    double xmax =  2.5;             // angstrom
+    char *outputfile = "/dev/stdout";
 
     // static declaration
     double EvalTerm1, EvalTerm2;
     double E0, E1, E2, E3, E4, E5;
-    double N0, N1, N2, N3, N4, N5, lambda;
-    double z, x;
+
+    double N0, N1, N2, N3, N4, N5;
+    double x, z, expz, lambda;
+    double powz0, powz1, powz2, powz3, powz4, powz5;
     double L0, L1, L2, L3, L4, L5;
+    FILE *fd = fopen(outputfile, "w");
 
     // constants
     double pi       = 3.14159265358979323846;
@@ -54,38 +58,60 @@ int main(int argc, char **argv){
     N4 = sqrt(( 48.0*lambda -  216.0)/gsl_sf_gamma(2.0*lambda - 4));  //printf("N4 = %lf\n", N4);
     N5 = sqrt((240.0*lambda - 1320.0)/gsl_sf_gamma(2.0*lambda - 5));  //printf("N5 = %lf\n", N5);
 
-    double evecterm2; 
-    double evecterm30, evecterm31, evecterm32, evecterm33, evecterm34, evecterm35;
+    fprintf(fd, "#EvalTerm1 = %lf kJ/mol\n", EvalTerm1);
+    fprintf(fd, "#EvalTerm2 = %lf kJ/mol\n", EvalTerm2);
+    fprintf(fd, "#lambda = %lf\n", lambda);
+    fprintf(fd, "#N0 = % 22.20lf\n", N0);
+    fprintf(fd, "#N2 = % 22.20lf\n", N2);
+    fprintf(fd, "#N3 = % 22.20lf\n", N3);
+    fprintf(fd, "#N4 = % 22.20lf\n", N4);
+    fprintf(fd, "#N5 = % 22.20lf\n", N5);
+
+    // Calculate E+Psi and output data
+    //fprintf(fd, "# x\tV(x)\tE0+Psi0\tE1+Psi1\tE2+Psi2\tE3+Psi3\tE4+Psi4\tE5+Psi5\n");
+    fprintf(fd, "#  x\t\t");
+    fprintf(fd, " V(x)\t\t\t");
+    fprintf(fd, " E0+Psi0\t");
+    fprintf(fd, " E1+Psi1\t");
+    fprintf(fd, " E2+Psi2\t");
+    fprintf(fd, " E3+Psi3\t");
+    fprintf(fd, " E4+Psi4\t");
+    fprintf(fd, " E5+Psi5\t");
+    fprintf(fd, "\n");
     for(x = xmin; x <= xmax; x += dx){
         
         z = 2*lambda*exp(-alpha*x); //no dimension
         
-        evecterm2 = exp(-0.5*z);
+        expz = exp(-0.5*z);
     
-        evecterm30 = pow(z, lambda-0.5);
-        evecterm31 = pow(z, lambda-1.5);
-        evecterm32 = pow(z, lambda-2.5);
-        evecterm33 = pow(z, lambda-3.5);
-        evecterm34 = pow(z, lambda-4.5);
-        evecterm35 = pow(z, lambda-5.5);
+        // z^(lambda-n-0.5)
+        powz0 = pow(z, lambda-0.5);
+        powz1 = pow(z, lambda-1.5);
+        powz2 = pow(z, lambda-2.5);
+        powz3 = pow(z, lambda-3.5);
+        powz4 = pow(z, lambda-4.5);
+        powz5 = pow(z, lambda-5.5);
 
+        // calculation of Laguerre polynomials through recursion formula: 
+        // Ln(z,a)=( (2*(n-1) + 1 + a - z)*L(n-1) - (n - 1 + a)*L(n-2) )/n;
+        // L0 = 1; L1 = 1+a-z;
+        // in the given case:   a = 2*lambda-2*1-1;
         L0 = 1;
-        L1 = 2*lambda - 2 - z;
-        //Ln=( (2*(n-1) + 1 + 2*lambda-2*n-1 - z)*L(n-1) - (n - 1 + 2*lambda-2*n-1)*L(n-2) )/n;
+        L1 = 1 + 2*lambda-2*1-1 - z;
         L2 = ( (2*(2-1) + 1 + 2*lambda-2*2-1 - z)*L1 - (2 - 1 + 2*lambda-2*2-1)*L0 )/2;
         L3 = ( (2*(3-1) + 1 + 2*lambda-2*3-1 - z)*L2 - (3 - 1 + 2*lambda-2*3-1)*L1 )/3;
         L4 = ( (2*(4-1) + 1 + 2*lambda-2*4-1 - z)*L3 - (4 - 1 + 2*lambda-2*4-1)*L2 )/4;
         L5 = ( (2*(5-1) + 1 + 2*lambda-2*5-1 - z)*L4 - (5 - 1 + 2*lambda-2*5-1)*L3 )/5;
 
-        printf("%lf  ", x);
-        printf("%lf  ", D*(1.0 - 2.0*exp(-alpha*x) + exp(-2.0*alpha*x)));
-        printf("%lf  ", E0 + N0 * evecterm2 * evecterm30 * L0);
-        printf("%lf  ", E1 + N1 * evecterm2 * evecterm31 * L1);
-        printf("%lf  ", E2 + N2 * evecterm2 * evecterm32 * L2);
-        printf("%lf  ", E3 + N3 * evecterm2 * evecterm33 * L3);
-        printf("%lf  ", E4 + N4 * evecterm2 * evecterm34 * L4);
-        printf("%lf  ", E5 + N5 * evecterm2 * evecterm35 * L5);
-        printf("\n");                                   
+        fprintf(fd, "% 8.8lf\t", x);
+        fprintf(fd, "% 19.8lf\t", D*(1.0 - 2.0*exp(-alpha*x) + exp(-2.0*alpha*x)));
+        fprintf(fd, "% 15.8lf\t", E0 + N0 * expz * powz0 * L0);
+        fprintf(fd, "% 15.8lf\t", E1 + N1 * expz * powz1 * L1);
+        fprintf(fd, "% 15.8lf\t", E2 + N2 * expz * powz2 * L2);
+        fprintf(fd, "% 15.8lf\t", E3 + N3 * expz * powz3 * L3);
+        fprintf(fd, "% 15.8lf\t", E4 + N4 * expz * powz4 * L4);
+        fprintf(fd, "% 15.8lf\t", E5 + N5 * expz * powz5 * L5);
+        fprintf(fd, "\n");                                   
     }
 
     return 0;
