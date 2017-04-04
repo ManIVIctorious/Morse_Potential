c4b6f721d6825bbc1cb82f4655535cf6c646e7d7
Manuel <manivictorious@gmail.com>
2017-04-04 11:44:00 +0200

    add excitation flag and some minor code improvements
        - add --excit flag, if set showes excitation energies in cm^-1
        - replace double pi by M_PI since math.h is already set
    
        - break a lot of if else checks by adding a energy conversion factor
            and a dimension string:
            e.g.:
            if(kcal_flag == 1){
                fprintf(fd, "% 26.18lf kcal/mol\n", E/4.184);
            }else{
                fprintf(fd, "% 26.18lf kJ/mol\n", E);
            }
    
            got replaced by:
            if(kcal_flag == 1){
                econv_factor = 4.184;
                dimension = kcal/mol;
            }
            fprintf(fd, "% 26.18lf %s\n", E/econv_factor, dimension);

--- MorsePotential.c
+++ MorsePotential.c
@@ -20,6 +20,9 @@ int main(int argc, char **argv){
     char *outputfile = "/dev/stdout";
     static int kcal_flag = 0; // set flag to kcal/mol
     static int evecs_flag = 0;
+    static int excit_flag = 0;
+    double conv_factor = 1.0;
+    char *dimension = "kJ/mol";
     int numberofeigenstates = 6;
 
     int c;
@@ -28,6 +31,7 @@ int main(int argc, char **argv){
             /* These options set a flag. */
             {"kcal",  no_argument, &kcal_flag , 1},
             {"evecs", no_argument, &evecs_flag, 1},
+            {"excit", no_argument, &excit_flag, 1},
             /* These options don’t set a flag.
                We distinguish them by their indices. */
             {"help",                  no_argument,       0, 'h'},
@@ -77,8 +81,9 @@ int main(int argc, char **argv){
                 printf("[--kcal]");
                 printf("\n\t\t\t");
                 printf("[--reduced-mass|-m <value>]");
+                printf("[--excit]");
                 printf("\n\n");
-                
+
                 printf("-h, --help\t\t\tShow this help dialogue\n");
                 printf("-a, --xmin\t\t\tSet minimum x-value\n");
                 printf("-b, --xmax\t\t\tSet maximum x-value\n");
@@ -89,7 +94,8 @@ int main(int argc, char **argv){
                 printf("-o, --output-file\t\tName of output file\n");
                 printf("-n, --number-of-eigenstates\tNumber of calculated eigenstates\n");
                 printf("    --kcal\t\t\tOutput in kcal/mol\n");
-                printf("    --evecs\t\t\tOutput eigenvectors directly (don't add evals)");
+                printf("    --evecs\t\t\tOutput eigenvectors directly (don't add evals)\n");
+                printf("    --excit\t\t\tOutput excitation state energy in cm^-1\n");
                 printf("\n");
                 exit (0);
 
@@ -132,7 +138,7 @@ int main(int argc, char **argv){
     }
 
     // static declaration
-    int n;
+    int m, n;
     double EvalTerm1, EvalTerm2;
     double E[numberofeigenstates];
     double N[numberofeigenstates];
@@ -141,22 +147,22 @@ int main(int argc, char **argv){
     double x, z, expz, lambda;
     FILE *fd = fopen(outputfile, "w");
 
+
     // constants
-    double pi       = 3.1415926535897932384626433832795;
-    double planck   = 6.626070040E-34;          // Js
-    double avogadro = 6.022140857E23;           // 1/mol
+    double planck     = 6.626070040E-34;          // Js
+    double avogadro   = 6.022140857E23;           // 1/mol
+    double lightspeed = 299792458;                // m/s
+
+    if(kcal_flag == 1){
+        conv_factor = 4.184;
+        dimension = "kcal/mol";
+    }
 
     // begin calculation of Eigenvalues E
     // eval = planck*alpha/(2*pi)*sqrt(2*D/mu)       * (n + 0.5)            term1*(n+0.5)
     //      - planck*planck*alpha*alpha/(8*pi*pi*mu) * (n*(n + 1) + 0.25)   term2*(n*n+n+0.25)
-    EvalTerm1 =           1.0E10/(2.0*pi)*avogadro * planck*alpha*sqrt(2.0*D/mu);   // kJ/mol
-    EvalTerm2 = 1.0E20/(8.0*pi*pi)*avogadro*avogadro * planck*planck*alpha*alpha/mu;  // kJ/mol
-
-    // convert to kcal/mol
-    if(kcal_flag == 1){
-        EvalTerm1 /= 4.184;
-        EvalTerm2 /= 4.184;
-    }
+    EvalTerm1 =           1.0E10/(2.0*M_PI)*avogadro * planck*alpha*sqrt(2.0*D/mu);   // kJ/mol
+    EvalTerm2 = 1.0E20/(8.0*M_PI*M_PI)*avogadro*avogadro * planck*planck*alpha*alpha/mu;  // kJ/mol
 
     // get eigenvalues
     for(n=0; n<numberofeigenstates; ++n){
@@ -165,7 +171,7 @@ int main(int argc, char **argv){
 
     // begin calculation of eigenvectors Psi:
     // evec = N(n) * exp(-0.5*z) * z^(lambda-n-0.5) * L(n,x,lambda)
-    lambda = 1.0/(1.0E10*avogadro) * sqrt(2.0*mu*D)/(alpha*planck/(2.0*pi));      // dimensionless 
+    lambda = 1.0/(1.0E10*avogadro) * sqrt(2.0*mu*D)/(alpha*planck/(2.0*M_PI));      // dimensionless
 
     for(n=0; n<numberofeigenstates; ++n){
         N[n] = sqrt(((double)factorial(n)*(2.0*lambda-2.0*(double)n-1.0))/gsl_sf_gamma(2.0*lambda - (double)n));
@@ -176,20 +182,24 @@ int main(int argc, char **argv){
     fprintf(fd, "# D:            %+18.12e kJ/mol\n", D);
     fprintf(fd, "# alpha:        %+18.12e 1/angstrom\n", alpha);
     fprintf(fd, "# reduced mass: %+18.12e g/mol\n", mu);
-    if(kcal_flag==1){
-    fprintf(fd, "# EvalTerm1:    %+18.12e kcal/mol\n", EvalTerm1);
-    fprintf(fd, "# EvalTerm2:    %+18.12e kcal/mol\n", EvalTerm2);
-    }else{
-    fprintf(fd, "# EvalTerm1:    %+18.12e kJ/mol\n", EvalTerm1);
-    fprintf(fd, "# EvalTerm2:    %+18.12e kJ/mol\n", EvalTerm2);
-    }
+    fprintf(fd, "# EvalTerm1:    %+18.12e %s\n", EvalTerm1/conv_factor, dimension);
+    fprintf(fd, "# EvalTerm2:    %+18.12e %s\n", EvalTerm2/conv_factor, dimension);
     fprintf(fd, "# lambda:       %+18.12e\n", lambda);
     for(n=0; n<numberofeigenstates; ++n){
-        if(kcal_flag == 1){
-            fprintf(fd, "#\tN%02d = % 26.20lf\t E%02d = % 26.20lf kcal/mol\n", n, N[n], n, E[n]);
-        }else{
-            fprintf(fd, "#\tN%02d = % 26.20lf\t E%02d = % 26.20lf kJ/mol\n", n, N[n], n, E[n]);
+        fprintf(fd, "#\tN%02d = % 26.20lf\t E%02d = % 26.20lf %s\n", n, N[n], n, E[n]/conv_factor, dimension);
+    }
+
+    // if excitation flag is set print excitation energies in cm^-1
+    if(excit_flag == 1){
+        fprintf(fd, "#\n");
+        fprintf(fd, "# Excitation energies in cm^-1:\n#");
+        for(n = 1; n < numberofeigenstates; ++n){
+            for(m = 0; m < n; ++m){
+                fprintf(fd, "\tEx%d%d = % 18.12lf", m, n, (E[n]-E[m])/avogadro/planck/lightspeed*10);
+            }
+            fprintf(fd, "\n#");
         }
+        fprintf(fd, "\n#");
     }
 
     // Calculate E+Psi and output data
@@ -199,17 +209,17 @@ int main(int argc, char **argv){
     }
     fprintf(fd, "\n");
     for(x = xmin; x <= xmax; x += dx){
-        
+
         z = 2.0*lambda*exp(-alpha*x); //no dimension
-        
+
         expz = exp(-0.5*z);
-    
+
         // z^(lambda-n-0.5)
         for(n=0; n<numberofeigenstates; ++n){
             powz[n] = pow(z, lambda - (double)n - 0.5);
         }
 
-        // calculation of Laguerre polynomials through recursion formula: 
+        // calculation of Laguerre polynomials through recursion formula:
         // Ln(z,a)=( (2*(n-1) + 1 + a - z)*L(n-1) - (n - 1 + a)*L(n-2) )/n;
         // L[0] = 1; L[1] = 1+a-z;
         // in the given case:   a = 2*lambda-2*n-1;
@@ -220,22 +230,18 @@ int main(int argc, char **argv){
         }
 
         fprintf(fd, "% 26.18lf  ", x);
-        if(kcal_flag == 1){
-            fprintf(fd, "% 26.18e  ", D*(1.0 - 2.0*exp(-alpha*x) + exp(-2.0*alpha*x))/4.184);
-        }else{
-            fprintf(fd, "% 26.18e  ", D*(1.0 - 2.0*exp(-alpha*x) + exp(-2.0*alpha*x)));
-        }
+        fprintf(fd, "% 26.18e  ", D*(1.0 - 2.0*exp(-alpha*x) + exp(-2.0*alpha*x))/conv_factor);
 
-        if(evecs_flag == 0){
-            for(n=0; n<numberofeigenstates; ++n){
-                fprintf(fd, "% 26.18lf  ", E[n] + N[n] * expz * powz[n] * L[n]);
-            }
-        } else{
+        if(evecs_flag == 1){
             for(n=0; n<numberofeigenstates; ++n){
                 fprintf(fd, "% 26.18lf  ", N[n] * expz * powz[n] * L[n]);
             }
+        }else{
+            for(n=0; n<numberofeigenstates; ++n){
+                fprintf(fd, "% 26.18lf  ", E[n]/conv_factor + N[n] * expz * powz[n] * L[n]);
+            }
         }
-        fprintf(fd, "\n");                                   
+        fprintf(fd, "\n");
     }
     fclose(fd);
 
